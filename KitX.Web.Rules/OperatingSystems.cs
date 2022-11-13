e13df82a6d69484a923a7b234b83add4020cb102
Dynesshely <catrol@qq.com>
2022-11-13 11:04:16 +0800

    💾 Feat: 调整了OS列表顺序, 添加IoT类型

--- KitX.Web.Rules/OperatingSystems.cs
+++ KitX.Web.Rules/OperatingSystems.cs
@@ -2,7 +2,7 @@
 {
     public enum OperatingSystems
     {
-        Android, Browser, FreeBSD, IOS, Linux, MacCatalyst, MacOS, TvOS, WatchOS, Windows,
-        Unknown
+        Unknown = 0, Android = 1, Browser = 2, FreeBSD = 3, IOS = 4, Linux = 5, MacCatalyst = 6, MacOS = 7,
+        TvOS = 8, WatchOS = 9, Windows = 10, IoT = 11
     }
 }
