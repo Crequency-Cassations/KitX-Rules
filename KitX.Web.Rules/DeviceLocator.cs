64c2f8bf831f984d15ffbc2ed431c91277f1f6c5
Dynesshely <catrol@qq.com>
2022-11-26 09:56:56 +0800

    💾 Feat: Web.Rules 更新

---
+++ KitX.Web.Rules/DeviceLocator.cs
@@ -0,0 +1,25 @@
+﻿namespace KitX.Web.Rules
+{
+    public struct DeviceLocator
+    {
+        /// <summary>
+        /// 设备名称
+        /// </summary>
+        public string DeviceName { get; set; }
+
+        /// <summary>
+        /// 内网IPv4地址
+        /// </summary>
+        public string IPv4 { get; set; }
+
+        /// <summary>
+        /// 内网IPv6地址
+        /// </summary>
+        public string IPv6 { get; set; }
+
+        /// <summary>
+        /// 设备MAC地址
+        /// </summary>
+        public string DeviceMacAddress { get; set; }
+    }
+}
--- KitX.Web.Rules/Function.cs
+++ KitX.Web.Rules/Function.cs
@@ -18,9 +18,16 @@ namespace KitX.Web.Rules
         public Dictionary<string, string> DisplayNames { get; set; }
 
         /// <summary>
-        /// 参数列表, Value 为参数类型
+        /// 参数列表
+        /// key 为参数默认名称, 遵循变量命名法则
+        /// value 为参数显示名称, 用于显示在界面上
         /// </summary>
-        public Dictionary<Dictionary<string, string>, string> Parameters { get; set; }
+        public Dictionary<string, Dictionary<string, string>> Parameters { get; set; }
+
+        /// <summary>
+        /// 参数类型列表
+        /// </summary>
+        public List<string> ParametersType { get; set; }
 
         /// <summary>
         /// 是否有后继参数
