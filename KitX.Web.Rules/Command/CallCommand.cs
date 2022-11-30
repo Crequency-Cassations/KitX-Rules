14048974fb7c943737cda4b5da0f83afc02e758f
Dynesshely <catrol@qq.com>
2022-11-30 17:07:21 +0800

    💾 Feat: 准备自组网命令框架

---
+++ KitX.Web.Rules/Command/CallCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KitX.Web.Rules.Command
+{
+    public class CallCommand
+    {
+        public DeviceLocator Sender { get; set; }
+
+        public DeviceLocator Target { get; set; }
+
+        public string Command { get; set; }
+    }
+}
---
+++ KitX.Web.Rules/Command/CallbackCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KitX.Web.Rules.Command
+{
+    public class CallbackCommand
+    {
+        public DeviceLocator Sender { get; set; }
+
+        public DeviceLocator Target { get; set; }
+
+        public string Command { get; set; }
+    }
+}
