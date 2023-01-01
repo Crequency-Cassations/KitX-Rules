f132d330bd5d66703250e1bd7001f28b9df885ef
Dynesshely <catrol@qq.com>
2023-01-02 05:11:44 +0800

    💾 Feat: 抽象 Command 类

---
+++ KitX.Web.Rules/Command.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KitX.Web.Rules
+{
+    public class Command
+    {
+        public DeviceLocator Sender { get; set; }
+
+        public DeviceLocator Target { get; set; }
+
+        public int CallId { get; set; }
+
+        public int CallIdTTL { get; set; }
+
+        public DateTime SendTime { get; set; }
+
+        public string Request { get; set; }
+
+        public List<string> RequestArgs { get; set; }
+
+        public byte[] Body { get; set; }
+
+        public int BodyLength { get; set; }
+
+        public Dictionary<string, string> Tags { get; set; }
+    }
+}
